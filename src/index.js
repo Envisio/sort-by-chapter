ad2acc6c30b7d63d7cd96a8266984d43288bec8c
Barry Shao <bshao@envisio.com>
2019-11-12 12:43:21 -0800

    🐛

--- src/index.js
+++ src/index.js
@@ -20,7 +20,13 @@
 module.exports = (arr, options) => {
   if (options) {
     const attr = options.attribute;
-    return arr.sort((a, b) => (attr ? a[attr] : a).match(/(\d+\.*)+/)[0].localeCompare((attr ? b[attr] : b).match(/(\d+\.*)+/)[0], undefined, { numeric: true }));
+
+    return arr.sort((a, b) => {
+      if (attr && a[attr] && b[attr]) {
+        return a[attr].match(/(\d+\.*)+/)[0].localeCompare(b[attr].match(/(\d+\.*)+/)[0], undefined, { numeric: true });
+      }
+      return true;
+    });
   }
   return arr.sort((a, b) => a.match(/(\d+\.*)+/)[0].localeCompare(b.match(/(\d+\.*)+/)[0], undefined, { numeric: true }));
 };
